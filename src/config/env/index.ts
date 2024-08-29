6008078a9cccd02ceb4e4ad60687232b1b39eda6
brunoaclopes <brunolopes3000@gmail.com>
2024-08-29 22:55:07 +0100

    yarn lint --fix

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,7 +1,9 @@
+/* eslint-disable node/no-process-env, @typescript-eslint/naming-convention */
 import * as process from 'process'
 
 const env = {
   ...process.env,
 }
+/* eslint-enable node/no-process-env, @typescript-eslint/naming-convention */
 
 export default env
