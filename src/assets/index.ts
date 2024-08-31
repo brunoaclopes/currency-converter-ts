663a1651a1230888e9b5ec86a81cac7edfccb3c6
brunoaclopes <brunolopes3000@gmail.com>
2024-08-31 11:26:17 +0100

    Update assets

---
+++ src/assets/index.ts
@@ -0,0 +1,296 @@
+// Importing PNG assets
+import AED from './images/AED.png'
+import AED2x from './images/AED@2x.png'
+import AED3x from './images/AED@3x.png'
+import ARS from './images/ARS.png'
+import ARS2x from './images/ARS@2x.png'
+import ARS3x from './images/ARS@3x.png'
+import AUD from './images/AUD.png'
+import AUD2x from './images/AUD@2x.png'
+import AUD3x from './images/AUD@3x.png'
+import BAT from './images/BAT.png'
+import BAT2x from './images/BAT@2x.png'
+import BAT3x from './images/BAT@3x.png'
+import BCH from './images/BCH.png'
+import BCH2x from './images/BCH@2x.png'
+import BCH3x from './images/BCH@3x.png'
+import BRL from './images/BRL.png'
+import BRL2x from './images/BRL@2x.png'
+import BRL3x from './images/BRL@3x.png'
+import BTC from './images/BTC.png'
+import BTC2x from './images/BTC@2x.png'
+import BTC3x from './images/BTC@3x.png'
+import BTG from './images/BTG.png'
+import BTG2x from './images/BTG@2x.png'
+import BTG3x from './images/BTG@3x.png'
+import CAD from './images/CAD.png'
+import CAD2x from './images/CAD@2x.png'
+import CAD3x from './images/CAD@3x.png'
+import CHF from './images/CHF.png'
+import CHF2x from './images/CHF@2x.png'
+import CHF3x from './images/CHF@3x.png'
+import CNY from './images/CNY.png'
+import CNY2x from './images/CNY@2x.png'
+import CNY3x from './images/CNY@3x.png'
+import Crypto from './images/Crypto.png'
+import Crypto2x from './images/Crypto@2x.png'
+import Crypto3x from './images/Crypto@3x.png'
+import DASH from './images/DASH.png'
+import DASH2x from './images/DASH@2x.png'
+import DASH3x from './images/DASH@3x.png'
+import DKK from './images/DKK.png'
+import DKK2x from './images/DKK@2x.png'
+import DKK3x from './images/DKK@3x.png'
+import ETH from './images/ETH.png'
+import ETH2x from './images/ETH@2x.png'
+import ETH3x from './images/ETH@3x.png'
+import EUR from './images/EUR.png'
+import EUR2x from './images/EUR@2x.png'
+import EUR3x from './images/EUR@3x.png'
+import GBP from './images/GBP.png'
+import GBP2x from './images/GBP@2x.png'
+import GBP3x from './images/GBP@3x.png'
+import HKD from './images/HKD.png'
+import HKD2x from './images/HKD@2x.png'
+import HKD3x from './images/HKD@3x.png'
+import ILS from './images/ILS.png'
+import ILS2x from './images/ILS@2x.png'
+import ILS3x from './images/ILS@3x.png'
+import INR from './images/INR.png'
+import INR2x from './images/INR@2x.png'
+import INR3x from './images/INR@3x.png'
+import JPY from './images/JPY.png'
+import JPY2x from './images/JPY@2x.png'
+import JPY3x from './images/JPY@3x.png'
+import KES from './images/KES.png'
+import KES2x from './images/KES@2x.png'
+import KES3x from './images/KES@3x.png'
+import LTC from './images/LTC.png'
+import LTC2x from './images/LTC@2x.png'
+import LTC3x from './images/LTC@3x.png'
+import MXN from './images/MXN.png'
+import MXN2x from './images/MXN@2x.png'
+import MXN3x from './images/MXN@3x.png'
+import NOK from './images/NOK.png'
+import NOK2x from './images/NOK@2x.png'
+import NOK3x from './images/NOK@3x.png'
+import NZD from './images/NZD.png'
+import NZD2x from './images/NZD@2x.png'
+import NZD3x from './images/NZD@3x.png'
+import PHP from './images/PHP.png'
+import PHP2x from './images/PHP@2x.png'
+import PHP3x from './images/PHP@3x.png'
+import PLN from './images/PLN.png'
+import PLN2x from './images/PLN@2x.png'
+import PLN3x from './images/PLN@3x.png'
+import RVR from './images/RVR.png'
+import RVR2x from './images/RVR@2x.png'
+import RVR3x from './images/RVR@3x.png'
+import SEK from './images/SEK.png'
+import SEK2x from './images/SEK@2x.png'
+import SEK3x from './images/SEK@3x.png'
+import SGD from './images/SGD.png'
+import SGD2x from './images/SGD@2x.png'
+import SGD3x from './images/SGD@3x.png'
+import UAE from './images/UAE.png'
+import UAE2x from './images/UAE@2x.png'
+import UAE3x from './images/UAE@3x.png'
+import USD from './images/USD.png'
+import USD2x from './images/USD@2x.png'
+import USD3x from './images/USD@3x.png'
+import VOX from './images/VOX.png'
+import VOX2x from './images/VOX@2x.png'
+import VOX3x from './images/VOX@3x.png'
+import XAG from './images/XAG.png'
+import XAG2x from './images/XAG@2x.png'
+import XAG3x from './images/XAG@3x.png'
+import XAU from './images/XAU.png'
+import XAU2x from './images/XAU@2x.png'
+import XAU3x from './images/XAU@3x.png'
+import XPD from './images/XPD.png'
+import XPD2x from './images/XPD@2x.png'
+import XPD3x from './images/XPD@3x.png'
+import XPT from './images/XPT.png'
+import XPT2x from './images/XPT@2x.png'
+import XPT3x from './images/XPT@3x.png'
+import XRP from './images/XRP.png'
+import XRP2x from './images/XRP@2x.png'
+import XRP3x from './images/XRP@3x.png'
+
+// Importing SVG assets
+import appstore from './images/appstore.svg'
+import dropdownIcon from './images/dropdown-icon.svg'
+import logo from './images/logo.svg'
+import playstore from './images/playstore.svg'
+import qrCode from './images/qr-code.svg'
+import smallLogo from './images/small-logo.svg'
+
+// Exporting all assets
+export {
+  AED,
+  AED2x,
+  AED3x,
+  ARS,
+  ARS2x,
+  ARS3x,
+  AUD,
+  AUD2x,
+  AUD3x,
+  BAT,
+  BAT2x,
+  BAT3x,
+  BCH,
+  BCH2x,
+  BCH3x,
+  BRL,
+  BRL2x,
+  BRL3x,
+  BTC,
+  BTC2x,
+  BTC3x,
+  BTG,
+  BTG2x,
+  BTG3x,
+  CAD,
+  CAD2x,
+  CAD3x,
+  CHF,
+  CHF2x,
+  CHF3x,
+  CNY,
+  CNY2x,
+  CNY3x,
+  Crypto,
+  Crypto2x,
+  Crypto3x,
+  DASH,
+  DASH2x,
+  DASH3x,
+  DKK,
+  DKK2x,
+  DKK3x,
+  ETH,
+  ETH2x,
+  ETH3x,
+  EUR,
+  EUR2x,
+  EUR3x,
+  GBP,
+  GBP2x,
+  GBP3x,
+  HKD,
+  HKD2x,
+  HKD3x,
+  ILS,
+  ILS2x,
+  ILS3x,
+  INR,
+  INR2x,
+  INR3x,
+  JPY,
+  JPY2x,
+  JPY3x,
+  KES,
+  KES2x,
+  KES3x,
+  LTC,
+  LTC2x,
+  LTC3x,
+  MXN,
+  MXN2x,
+  MXN3x,
+  NOK,
+  NOK2x,
+  NOK3x,
+  NZD,
+  NZD2x,
+  NZD3x,
+  PHP,
+  PHP2x,
+  PHP3x,
+  PLN,
+  PLN2x,
+  PLN3x,
+  RVR,
+  RVR2x,
+  RVR3x,
+  SEK,
+  SEK2x,
+  SEK3x,
+  SGD,
+  SGD2x,
+  SGD3x,
+  UAE,
+  UAE2x,
+  UAE3x,
+  USD,
+  USD2x,
+  USD3x,
+  VOX,
+  VOX2x,
+  VOX3x,
+  XAG,
+  XAG2x,
+  XAG3x,
+  XAU,
+  XAU2x,
+  XAU3x,
+  XPD,
+  XPD2x,
+  XPD3x,
+  XPT,
+  XPT2x,
+  XPT3x,
+  XRP,
+  XRP2x,
+  XRP3x,
+  appstore,
+  dropdownIcon,
+  logo,
+  playstore,
+  qrCode,
+  smallLogo,
+}
+
+/* eslint-disable  @typescript-eslint/naming-convention */
+export const currencyImages = {
+  AED: { default: AED, '2x': AED2x, '3x': AED3x },
+  ARS: { default: ARS, '2x': ARS2x, '3x': ARS3x },
+  AUD: { default: AUD, '2x': AUD2x, '3x': AUD3x },
+  BAT: { default: BAT, '2x': BAT2x, '3x': BAT3x },
+  BCH: { default: BCH, '2x': BCH2x, '3x': BCH3x },
+  BRL: { default: BRL, '2x': BRL2x, '3x': BRL3x },
+  BTC: { default: BTC, '2x': BTC2x, '3x': BTC3x },
+  BTG: { default: BTG, '2x': BTG2x, '3x': BTG3x },
+  CAD: { default: CAD, '2x': CAD2x, '3x': CAD3x },
+  CHF: { default: CHF, '2x': CHF2x, '3x': CHF3x },
+  CNY: { default: CNY, '2x': CNY2x, '3x': CNY3x },
+  DASH: { default: DASH, '2x': DASH2x, '3x': DASH3x },
+  DKK: { default: DKK, '2x': DKK2x, '3x': DKK3x },
+  ETH: { default: ETH, '2x': ETH2x, '3x': ETH3x },
+  EUR: { default: EUR, '2x': EUR2x, '3x': EUR3x },
+  GBP: { default: GBP, '2x': GBP2x, '3x': GBP3x },
+  HKD: { default: HKD, '2x': HKD2x, '3x': HKD3x },
+  ILS: { default: ILS, '2x': ILS2x, '3x': ILS3x },
+  INR: { default: INR, '2x': INR2x, '3x': INR3x },
+  JPY: { default: JPY, '2x': JPY2x, '3x': JPY3x },
+  KES: { default: KES, '2x': KES2x, '3x': KES3x },
+  LTC: { default: LTC, '2x': LTC2x, '3x': LTC3x },
+  MXN: { default: MXN, '2x': MXN2x, '3x': MXN3x },
+  NOK: { default: NOK, '2x': NOK2x, '3x': NOK3x },
+  NZD: { default: NZD, '2x': NZD2x, '3x': NZD3x },
+  PHP: { default: PHP, '2x': PHP2x, '3x': PHP3x },
+  PLN: { default: PLN, '2x': PLN2x, '3x': PLN3x },
+  RVR: { default: RVR, '2x': RVR2x, '3x': RVR3x },
+  SEK: { default: SEK, '2x': SEK2x, '3x': SEK3x },
+  SGD: { default: SGD, '2x': SGD2x, '3x': SGD3x },
+  UAE: { default: UAE, '2x': UAE2x, '3x': UAE3x },
+  USD: { default: USD, '2x': USD2x, '3x': USD3x },
+  VOX: { default: VOX, '2x': VOX2x, '3x': VOX3x },
+  XAG: { default: XAG, '2x': XAG2x, '3x': XAG3x },
+  XAU: { default: XAU, '2x': XAU2x, '3x': XAU3x },
+  XPD: { default: XPD, '2x': XPD2x, '3x': XPD3x },
+  XPT: { default: XPT, '2x': XPT2x, '3x': XPT3x },
+  XRP: { default: XRP, '2x': XRP2x, '3x': XRP3x },
+}
+/* eslint-enable  @typescript-eslint/naming-convention */
