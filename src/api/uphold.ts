439c3990856bfad44dada91d21cc88cc33461114
brunoaclopes <brunolopes3000@gmail.com>
2024-08-31 11:27:29 +0100

    Add API methods and wrappers

---
+++ src/api/uphold.ts
@@ -0,0 +1,29 @@
+/* eslint-disable import/prefer-default-export */
+import SDK, { Ticker } from '@uphold/uphold-sdk-javascript'
+import { useQuery } from '@tanstack/react-query'
+import { Currencies } from '../constants'
+
+/* eslint-disable node/no-process-env, @typescript-eslint/naming-convention */
+const sdk = new SDK({
+  baseUrl: process.env.REACT_APP_BASE_URL || '',
+  clientId: process.env.REACT_APP_CLIENT_ID || '',
+  clientSecret: process.env.REACT_APP_CLIENT_SECRET || '',
+})
+/* eslint-enable node/no-process-env, @typescript-eslint/naming-convention */
+
+const getTicker = async (currency: string): Promise<Ticker[]> =>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ((await sdk.getTicker(currency)) as any).filter(
+    (ticker: Ticker) =>
+      ticker.currency !== currency &&
+      Currencies.some((c) => c === ticker.currency),
+  )
+
+export const useTicker = (currency: string) => {
+  const queryKey = ['ticker', currency]
+
+  return {
+    queryKey,
+    ...useQuery({ queryKey, queryFn: () => getTicker(currency) }),
+  }
+}
