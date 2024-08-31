e4b33997c5d09db9a3226933e9829d980267a386
brunoaclopes <brunolopes3000@gmail.com>
2024-08-31 11:28:20 +0100

    Add QueryClient to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { QueryClientProvider } from '@tanstack/react-query'
 import App from './app'
 import reportWebVitals from './report-web-vitals'
+import { QueryClient } from './api'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={QueryClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>,
 )
 
 // If you want to start measuring performance in your app, pass a function
